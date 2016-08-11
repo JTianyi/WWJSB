1f45a4948afa98f99c85f63564855d1e6ac68064
WoDeYan <12998899243@qq.com>
2016-08-11 16:55:20 +0800

    aaa

---
+++ app/src/main/java/com/example/dllo/wwj/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.dllo.wwj;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mac on 16/8/11.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //接活中
+    }
+}
